16882e7584010e5f9844413cb032c210f5e24e3c
wangsiyuan4 <290579475@qq.com>
2025-02-27 19:05:51 +0800

    fix: 服务添加可以以json列表形式启动多个

--- excel/index.js
+++ excel/index.js
@@ -1,15 +1,26 @@
 /*
  * @Author: wsy
  * @Date: 2024/1/16 10:00
- * @LastEditors: 
+ * @LastEditors:
  * @LastEditTime: 2024/1/16 10:00
  * @FilePath: nodejs-interface/excel
- * @Description: 
+ * @Description:
  */
 const XLSX = require('xlsx');
-const data = require('./json/data.json')
+const data1 = require('./json/data.json')
+const data2 = require('./json/data2.json')
+const data3 = require('./json/data3.json')
+const data4 = require('./json/data4.json')
 const workbook = XLSX.utils.book_new();
-const worksheet = XLSX.utils.aoa_to_sheet(data);
-XLSX.utils.book_append_sheet(workbook, worksheet, 'My Sheet');
-XLSX.writeFile(workbook, 'example.xlsx');
+const worksheet1 = XLSX.utils.aoa_to_sheet([["摄像头ID"]].concat(data1.map(item => [item])));
+const worksheet2 = XLSX.utils.aoa_to_sheet([["摄像头ID"]].concat(data2.map(item => [item])));
+const worksheet3 = XLSX.utils.aoa_to_sheet([["摄像头ID"]].concat(data3.map(item => [item])));
+const worksheet4 = XLSX.utils.aoa_to_sheet([["摄像头ID"]].concat(data4.map(item => [item])));
+// XLSX.utils.book_append_sheet(workbook, worksheet, 'My Sheet');
+// 将工作表添加到工作簿
+XLSX.utils.book_append_sheet(workbook, worksheet1, '1号宿舍');
+XLSX.utils.book_append_sheet(workbook, worksheet2, '2号宿舍');
+XLSX.utils.book_append_sheet(workbook, worksheet3, '3号宿舍');
+XLSX.utils.book_append_sheet(workbook, worksheet4, '4号宿舍');
+XLSX.writeFile(workbook, '摄像头列表.xlsx');
 console.log('Excel文件已生成');
--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const history = require("connect-history-api-fallback");
 const express = require("express");
 const opn = require('opn');
-const app = express();
+const fs = require('fs');
 require('dotenv').config({ path: 'config.env' });
 
 let httpPort = process.env.PORT;
@@ -10,31 +10,37 @@ const isOpen = process.env.OPEN_BROWSER;
 const fileUrl = process.env.WEB_PATH
 let list = []
 
-if(process.env.START_LIST && process.env.JSON_URL) {
-    list = require(process.env.JSON_URL)
-}
-
 function startServer(port) {
-
-    if(list?.length) {
-        list.forEach(item => {
-            let server = createServer({
-                routeUrl: item?.route,
-                port: item?.port,
-                isOpen: item?.isOpen,
-                fileUrl: item?.fileUrl
-            })
-            server.on("error", (err) => {
-                if(err.code === "EADDRINUSE") {
-                    console.log(`端口 ${ item?.port } 已被占用，尝试使用下一个可用端口`);
-                    startServer(item?.port + 1); // 尝试使用下一个端口
-                } else {
-                    console.error(err);
-                }
-            });
-        })
+    if(process.env.JSON_FILE && process.env.START_LIST) {
+        // 使用异步方法读取文件
+        fs.readFile(process.env.JSON_FILE, 'utf8', (err, data) => {
+            if(err) {
+                console.error('Error reading file:', err);
+            }
+            list = JSON.parse(data)
+            if(list?.length) {
+                list.forEach(item => {
+                    let server = createServer({
+                        app: express(),
+                        routeUrl: item?.route,
+                        port: item?.port,
+                        isOpen: item?.isOpen,
+                        fileUrl: item?.fileUrl,
+                        name: item?.name
+                    })
+                    server.on("error", (err) => {
+                        if(err.code === "EADDRINUSE") {
+                            console.log(`端口 ${ item?.port } 已被占用，尝试使用下一个可用端口`);
+                            startServer(item?.port + 1); // 尝试使用下一个端口
+                        } else {
+                            console.error(err);
+                        }
+                    });
+                })
+            }
+        });
     } else {
-        let server = createServer({ port, isOpen, routeUrl, fileUrl })
+        let server = createServer({ app: express(), port, isOpen, routeUrl, fileUrl })
         server.on("error", (err) => {
             if(err.code === "EADDRINUSE") {
                 console.log(`端口 ${ port } 已被占用，尝试使用下一个可用端口`);
@@ -46,12 +52,12 @@ function startServer(port) {
     }
 }
 
-function createServer({ routeUrl, port, isOpen, fileUrl }) {
+function createServer({ app, routeUrl, port, isOpen, fileUrl, name }) {
     app.use(routeUrl, history());
     app.use(routeUrl, express.static(fileUrl));
 
     return app.listen(port, '0.0.0.0', () => { // 监听所有 IP
-        console.log(`服务器启动成功，请访问 http://localhost:${ port + routeUrl } 或 http://${ getLocalIPAddress() }:${ port + routeUrl }`);
+        console.log(`服务器启动成功，请访问 http://localhost:${ port + routeUrl } 或 http://${ getLocalIPAddress() }:${ port + routeUrl } ---${name}`);
         if(isOpen) {
             opn(`http://127.0.0.1:${ port }`);
         }
